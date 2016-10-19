c6cab4432ed843cbb56e7678b0bc5a0734553409
Ivan Puddu <puddui@student.ethz.ch>
2016-10-19 19:55:51 +0200

    Implemented "peer muchain mutate" command
    Change-Id: Ibd0cd78c168b57bf33cd440b7839c07d5d5811a5
    Signed-off-by: Ivan Puddu <puddui@student.ethz.ch>

--- peer/muchain/mutate.go
+++ peer/muchain/mutate.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	pb "github.com/hyperledger/fabric/protos"
+	"github.com/hyperledger/fabric/peer/common"
+	"golang.org/x/net/context"
 )
 
 func mutateCmd() *cobra.Command {
+	muchainIssueMutantTxCmd.Flags().StringVarP(&txSetID, "txsetid", "d", "",
+		"The ID of the transactions set that should be mutated.")
+	muchainIssueMutantTxCmd.Flags().Uint64VarP(&blockNum, "blocknum", "b", 0,
+		"The block (as a positive number) where the new transaction of the set is stored.")
+	muchainIssueMutantTxCmd.Flags().Uint64VarP(&index, "index", "i", 0,
+		"The index (as a positive number) of the new active transaction (from the transactions belonging to the transactions set declared in the block given with --blocknum).")
+
 	return muchainIssueMutantTxCmd
 }
+
+var (
+	txSetID string
+	blockNum uint64
+	index uint64
+)
 
 var muchainIssueMutantTxCmd = &cobra.Command{
 	Use:       "mutate",
@@ -21,5 +37,36 @@ var muchainIssueMutantTxCmd = &cobra.Command{
 }
 
 func muchainIssueMutantTx(cmd *cobra.Command, args []string) error {
-	return nil
+	if !cmd.Flag("txsetid").Changed {
+		return fmt.Errorf("A valid transactions set id must be provided")
+	}
+	if !cmd.Flag("blocknum").Changed {
+		return  fmt.Errorf("A valid block number must be provided")
+	}
+	if !cmd.Flag("index").Changed {
+		return  fmt.Errorf("A valid index must be provided")
+	}
+
+	mutantSpec := &pb.MutantSpec{
+		TxSetID: txSetID,
+		BlockNum: blockNum,
+		Index: index,
+	}
+
+	devopsClient, err := common.GetDevopsClient(cmd)
+	if err != nil {
+		return fmt.Errorf("Error building the txSet: %s", err)
+	}
+
+	resp, err := devopsClient.Mutate(context.Background(), mutantSpec)
+	if err != nil {
+		return fmt.Errorf("Error issuing tx set: %s\n", err)
+	}
+
+	if resp.Status != pb.Response_SUCCESS {
+		return fmt.Errorf("No error returned while issuing the mutant transaction, but the mutation was not successful.")
+	}
+
+	logger.Infof("Successfully mutated state. Response message %s", string(resp.Msg))
+ 	return nil
 }
