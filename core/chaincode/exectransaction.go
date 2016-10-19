5f9d009f0b04c29f5653bdbdec75c4a44052139d
Ivan Puddu <puddui@student.ethz.ch>
2016-10-19 18:56:47 +0200

    Don't update the TxSetState for immutable txs
    Change-Id: Ibbac7e0bbf6ddd8b24eea598692b9041f405a446
    Signed-off-by: Ivan Puddu <puddui@student.ethz.ch>

--- core/chaincode/exectransaction.go
+++ core/chaincode/exectransaction.go
@@ -52,59 +52,71 @@ func Execute(ctxt context.Context, chain *ChaincodeSupport, inBlockTx *pb.InBloc
 
 	switch tx := inBlockTx.Transaction.(type) {
 	case *pb.InBlockTransaction_TransactionSet:
-		// Extend the set here and terminate the inBlockTransaction
-		var txSetExistedAlready = true
-		ledger.SetTxBegin(inBlockTx.Txid)
+
+		if len(tx.TransactionSet.Transactions) == 0 {
+			return nil, nil, fmt.Errorf("At least a transaction to execute should be provided.")
+		}
+
+		// Assume the set is a sigle transaction and take the first one of the set
+		defTx := tx.TransactionSet.Transactions[0]
+
 		txSetStValue, err := ledger.GetTxSetState(inBlockTx.Txid, true)
 		if err != nil {
-			ledger.SetTxFinished(inBlockTx.Txid, false)
 			return nil, nil, fmt.Errorf("Failed to retrieve the txSet state, txID: %s, err: %s.", inBlockTx.Txid, err)
 		}
-		if txSetStValue == nil {
-			txSetExistedAlready = false
-			txSetStValue = &pb.TxSetStateValue{}
-			txSetStValue.Index = &pb.TxSetIndex{BlockNr: nextBlockNr, InBlockIndex: tx.TransactionSet.DefaultInx}
-			txSetStValue.TxsInBlock = make(map[uint64]uint64)
-		}
-		txSetStValue.Nonce++
-		txInSet := uint64(len(tx.TransactionSet.Transactions))
-		txSetStValue.TxNumber += txInSet
-		txSetStValue.TxsInBlock[nextBlockNr] = txInSet
-		err = ledger.SetTxSetState(inBlockTx.Txid, txSetStValue)
-		if err != nil {
-			ledger.SetTxFinished(inBlockTx.Txid, false)
-			return nil, nil, fmt.Errorf("Unable to create the state for the new set. Error: %s", err)
-		}
-		ledger.SetTxFinished(inBlockTx.Txid, true)
+		if len(inBlockTx.GetTransactionSet().Transactions) > 1 || txSetStValue != nil {
+			// Update the tx set state. This is done only for transactions set with more than one transaction,
+			// or if the current tx is an extension of an already existing set).
+			var txSetExistedAlready = true
+			ledger.SetTxBegin(inBlockTx.Txid)
+			if txSetStValue == nil {
+				txSetExistedAlready = false
+				txSetStValue = &pb.TxSetStateValue{}
+				txSetStValue.Index = &pb.TxSetIndex{BlockNr: nextBlockNr, InBlockIndex: tx.TransactionSet.DefaultInx}
+				txSetStValue.TxsInBlock = make(map[uint64]uint64)
+			}
+			txSetStValue.Nonce++
+			txInSet := uint64(len(tx.TransactionSet.Transactions))
+			txSetStValue.TxNumber += txInSet
+			txSetStValue.TxsInBlock[nextBlockNr] = txInSet
+			err = ledger.SetTxSetState(inBlockTx.Txid, txSetStValue)
+			if err != nil {
+				ledger.SetTxFinished(inBlockTx.Txid, false)
+				return nil, nil, fmt.Errorf("Unable to create the state for the new set. Error: %s", err)
+			}
+			ledger.SetTxFinished(inBlockTx.Txid, true)
 
-		if txSetExistedAlready {
-			// The default transaction did not change, hence there is no need to execute the default transaction
-			return nil, nil, err
+			if txSetExistedAlready {
+				// The default transaction cannot be changed with a set extension, hence there is no need to re-execute the default transaction here
+				return nil, nil, err
+			}
+
+			if txSetStValue.Index.BlockNr != nextBlockNr {
+				// The default transaction is not the one of this block
+				// do not execute it
+				return nil, nil, err
+			}
+			// Use this as default transaction since this was a transactions set
+			defTx = tx.TransactionSet.Transactions[txSetStValue.Index.InBlockIndex]
 		}
 
-		if txSetStValue.Index.BlockNr != nextBlockNr {
-			// The default transaction is not the one of this block
-			// do not execute it
-			return nil, nil, err
-		}
-		t := tx.TransactionSet.Transactions[txSetStValue.Index.InBlockIndex]
-		if t.Type == pb.ChaincodeAction_CHAINCODE_DEPLOY {
-			_, err := chain.Deploy(ctxt, t)
+		if defTx.Type == pb.ChaincodeAction_CHAINCODE_DEPLOY {
+			_, err := chain.Deploy(ctxt, defTx)
 			if err != nil {
 				return nil, nil, fmt.Errorf("Failed to deploy chaincode spec(%s)", err)
 			}
 
 			//launch and wait for ready
-			markTxBegin(ledger, t)
-			_, _, err = chain.Launch(ctxt, t)
+			markTxBegin(ledger, defTx)
+			_, _, err = chain.Launch(ctxt, defTx)
 			if err != nil {
-				markTxFinish(ledger, t, false)
+				markTxFinish(ledger, defTx, false)
 				return nil, nil, fmt.Errorf("%s", err)
 			}
-			markTxFinish(ledger, t, true)
-		} else if t.Type == pb.ChaincodeAction_CHAINCODE_INVOKE || t.Type == pb.ChaincodeAction_CHAINCODE_QUERY {
+			markTxFinish(ledger, defTx, true)
+		} else if defTx.Type == pb.ChaincodeAction_CHAINCODE_INVOKE || defTx.Type == pb.ChaincodeAction_CHAINCODE_QUERY {
 			//will launch if necessary (and wait for ready)
-			cID, cMsg, err := chain.Launch(ctxt, t)
+			cID, cMsg, err := chain.Launch(ctxt, defTx)
 			if err != nil {
 				return nil, nil, fmt.Errorf("Failed to launch chaincode spec(%s)", err)
 			}
@@ -125,49 +137,49 @@ func Execute(ctxt context.Context, chain *ChaincodeSupport, inBlockTx *pb.InBloc
 			}
 
 			var ccMsg *pb.ChaincodeMessage
-			if t.Type == pb.ChaincodeAction_CHAINCODE_INVOKE {
-				ccMsg, err = createTransactionMessage(t.Txid, cMsg)
+			if defTx.Type == pb.ChaincodeAction_CHAINCODE_INVOKE {
+				ccMsg, err = createTransactionMessage(defTx.Txid, cMsg)
 				if err != nil {
 					return nil, nil, fmt.Errorf("Failed to transaction message(%s)", err)
 				}
 			} else {
-				ccMsg, err = createQueryMessage(t.Txid, cMsg)
+				ccMsg, err = createQueryMessage(defTx.Txid, cMsg)
 				if err != nil {
 					return nil, nil, fmt.Errorf("Failed to query message(%s)", err)
 				}
 			}
 
-			markTxBegin(ledger, t)
-			resp, err := chain.Execute(ctxt, chaincode, ccMsg, timeout, t)
+			markTxBegin(ledger, defTx)
+			resp, err := chain.Execute(ctxt, chaincode, ccMsg, timeout, defTx)
 			if err != nil {
 				// Rollback transaction
-				markTxFinish(ledger, t, false)
+				markTxFinish(ledger, defTx, false)
 				return nil, nil, fmt.Errorf("Failed to execute transaction or query(%s)", err)
 			} else if resp == nil {
 				// Rollback transaction
-				markTxFinish(ledger, t, false)
-				return nil, nil, fmt.Errorf("Failed to receive a response for (%s)", t.Txid)
+				markTxFinish(ledger, defTx, false)
+				return nil, nil, fmt.Errorf("Failed to receive a response for (%s)", defTx.Txid)
 			} else {
 				if resp.ChaincodeEvent != nil {
 					resp.ChaincodeEvent.ChaincodeID = chaincode
-					resp.ChaincodeEvent.TxID = t.Txid
+					resp.ChaincodeEvent.TxID = defTx.Txid
 				}
 
 				if resp.Type == pb.ChaincodeMessage_COMPLETED || resp.Type == pb.ChaincodeMessage_QUERY_COMPLETED {
 					// Success
-					markTxFinish(ledger, t, true)
+					markTxFinish(ledger, defTx, true)
 					return resp.Payload, resp.ChaincodeEvent, nil
 				} else if resp.Type == pb.ChaincodeMessage_ERROR || resp.Type == pb.ChaincodeMessage_QUERY_ERROR {
 					// Rollback transaction
-					markTxFinish(ledger, t, false)
+					markTxFinish(ledger, defTx, false)
 					return nil, resp.ChaincodeEvent, fmt.Errorf("Transaction or query returned with failure: %s", string(resp.Payload))
 				}
-				markTxFinish(ledger, t, false)
-				return resp.Payload, nil, fmt.Errorf("receive a response for (%s) but in invalid state(%d)", t.Txid, resp.Type)
+				markTxFinish(ledger, defTx, false)
+				return resp.Payload, nil, fmt.Errorf("receive a response for (%s) but in invalid state(%d)", defTx.Txid, resp.Type)
 			}
 
 		} else {
-			err = fmt.Errorf("Invalid transaction type %s", t.Type.String())
+			err = fmt.Errorf("Invalid transaction type %s", defTx.Type.String())
 		}
 		return nil, nil, err
 	case *pb.InBlockTransaction_MutantTransaction:
