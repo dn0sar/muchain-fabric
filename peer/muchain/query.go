45aa4fb90c4a134abf9aca71f80690f8feb7fc38
Ivan Puddu <puddui@student.ethz.ch>
2016-10-20 16:44:22 +0200

    Implemented client side Tx Set State Query
    Change-Id: I2ee8fd056bbaf761012a85ea58461bbb48ec2ed8
    Signed-off-by: Ivan Puddu <puddui@student.ethz.ch>

--- peer/muchain/query.go
+++ peer/muchain/query.go
@@ -1,7 +1,13 @@
 package muchain
 
 import (
+	"fmt"
+	"errors"
 	"github.com/spf13/cobra"
+
+	pb "github.com/hyperledger/fabric/protos"
+	"github.com/hyperledger/fabric/peer/common"
+	"golang.org/x/net/context"
 )
 
 func queryState() *cobra.Command {
@@ -10,9 +16,9 @@ func queryState() *cobra.Command {
 }
 
 var muchainQueryTxSetStateCmd = &cobra.Command{
-	Use:       "query-state",
-	Short:     "Queries the state of a transactions set.",
-	Long:      `Queries the state of a transactions set.`,
+	Use:       "query-state 'tx-set-id'",
+	Short:     "Queries the state of the transactions set given as argument.",
+	Long:      `Queries the state of the transactions set given as argument.`,
 	ValidArgs: []string{"1"},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return muchainQueryTxSetState(cmd, args)
@@ -20,5 +26,34 @@ var muchainQueryTxSetStateCmd = &cobra.Command{
 }
 
 func muchainQueryTxSetState(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("Exactly one argument must be provided. The tx set id of the tx set state to query.")
+	}
+
+	querySpec := &pb.MutantSpec{
+		TxSetID: args[0],
+	}
+
+	devopsClient, err := common.GetDevopsClient(cmd)
+	if err != nil {
+		return fmt.Errorf("Error building the txSet: %s", err)
+	}
+
+	resp, err := devopsClient.QueryTxSetState(context.Background(), querySpec)
+	if err != nil {
+		return fmt.Errorf("Error query the tx set state: %s\n", err)
+	}
+
+	if resp.Status != pb.Response_SUCCESS {
+		return fmt.Errorf("No error returned while querying the tx set state, but the response status is not successfull. Status: %#v", resp.Status)
+	}
+
+	txSetState, err := pb.UnmarshalTxSetStateValue(resp.Msg)
+	if err != nil {
+		return errors.New("Query successfull, but unable to unmarshal the response.")
+	}
+
+	logger.Infof("Successfully queried state. Result: %#v", txSetState)
+
 	return nil
 }
